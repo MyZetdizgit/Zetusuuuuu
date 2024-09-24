b79f1d5d90c2aefb8c4ffdc60ef2afa2bf0d246a
MyZetdizgit <bambizazizo@gmail.com>
2024-09-24 12:26:04 +0100

    Update showPrefix.js

--- plugins/onMessage/showPrefix.js
+++ plugins/onMessage/showPrefix.js
@@ -1,6 +1,6 @@
 const langData = {
     "en_US": {
-        "prefix.get": "Prefix is: {prefix}"
+        "prefix.get": "┏𝙋𝙍𝙀𝙁𝙄𝙓 : ⇛ {prefix} ⇚\n☁️ 𝘛𝘢𝘱𝘦 {prefix}𝘩𝘦𝘭𝘱 𝘱𝘰𝘶𝘳 𝘷𝘰𝘪𝘳 𝘭𝘦𝘴 𝘤𝘰𝘮𝘮𝘢𝘯𝘥𝘦𝘴 𝘥𝘪𝘴𝘱𝘰𝘯𝘪𝘣𝘭𝘦𝘴\n┗━━━━⌾\n                  ━━◊━━     \n 𝗭𝗲𝘁-𝗫 𝗽𝗼𝘂𝗿 𝘃𝗼𝘂𝘀 𝗦𝗲𝗿𝘃𝗶𝗿"
     },
     "vi_VN": {
         "prefix.get": "Prefix hiện tại là: {prefix}"
@@ -8,7 +8,7 @@ const langData = {
 }
 
 function onCall({ message, getLang, data }) {
-    if (message.body == "prefix" && message.senderID != global.botID) {
+    if (message.body == "Prefix" && message.senderID != global.botID) {
         message.reply(getLang("prefix.get", {
             prefix: data?.thread?.data?.prefix || global.config.PREFIX
         }));
